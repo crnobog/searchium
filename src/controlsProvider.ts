18f6e981fc06ff266a77dfbfd3e5f634971db2bc
Robert Millar <robert.millar@cantab.net>
2023-03-25 13:37:20 -0700

    separate ts config for web/node downgrade ts because of fast-foundation

--- src/controlsProvider.ts
+++ src/controlsProvider.ts
@@ -1,5 +1,18 @@
 import * as vscode from 'vscode';
 
+export function getNonce() {
+    let text = "";
+    const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    for (let i = 0; i < 32; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+}
+
+export function getUri(webview: vscode.Webview, extensionUri: vscode.Uri, pathList: string[]) {
+    return webview.asWebviewUri(vscode.Uri.joinPath(extensionUri, ...pathList));
+}
+
 export class ControlsProvider implements vscode.WebviewViewProvider {
     constructor(private readonly extensionUri: vscode.Uri) { }
     public resolveWebviewView(
@@ -11,10 +24,12 @@ export class ControlsProvider implements vscode.WebviewViewProvider {
             enableScripts: true
         };
 
-        webviewView.webview.html = this.getWebViewContent();
+        webviewView.webview.html = this.getWebViewContent(webviewView.webview, this.extensionUri);
     }
 
-    private getWebViewContent(): string {
+    private getWebViewContent(webview: vscode.Webview, extensionUri: vscode.Uri): string {
+        const webviewUri = getUri(webview, extensionUri, ["out", "webview.js"]);
+        const nonce = getNonce();
         return /*html*/ `
           <!DOCTYPE html>
           <html lang="en">
@@ -25,6 +40,8 @@ export class ControlsProvider implements vscode.WebviewViewProvider {
             </head>
             <body>
               <h1>Hello World!</h1>
+              <vscode-text-field>Text Field Label</vscode-text-field>
+              <script type="module" nonce="${nonce}" src="${webviewUri}"></script>
             </body>
           </html>
         `;
--- src/webview/main.ts
+++ src/webview/main.ts
@@ -1,1 +1,51 @@
+import {
+    provideVSCodeDesignSystem,
+    Checkbox,
+    DataGrid,
+    vsCodeBadge,
+    vsCodeButton,
+    vsCodeCheckbox,
+    vsCodeDataGrid,
+    vsCodeDataGridCell,
+    vsCodeDataGridRow,
+    vsCodeDivider,
+    vsCodeDropdown,
+    vsCodeLink,
+    vsCodeOption,
+    vsCodePanels,
+    vsCodePanelTab,
+    vsCodePanelView,
+    vsCodeProgressRing,
+    vsCodeRadio,
+    vsCodeRadioGroup,
+    vsCodeTag,
+    vsCodeTextArea,
+    vsCodeTextField,
+} from "@vscode/webview-ui-toolkit";
+
+// In order to use the Webview UI Toolkit web components they
+// must be registered with the browser (i.e. webview) using the
+// syntax below.
+provideVSCodeDesignSystem().register(
+    vsCodeBadge(),
+    vsCodeButton(),
+    vsCodeCheckbox(),
+    vsCodeDataGrid(),
+    vsCodeDataGridCell(),
+    vsCodeDataGridRow(),
+    vsCodeDivider(),
+    vsCodeDropdown(),
+    vsCodeLink(),
+    vsCodeOption(),
+    vsCodePanels(),
+    vsCodePanelTab(),
+    vsCodePanelView(),
+    vsCodeProgressRing(),
+    vsCodeRadio(),
+    vsCodeRadioGroup(),
+    vsCodeTag(),
+    vsCodeTextArea(),
+    vsCodeTextField()
+);
+
 export { };
