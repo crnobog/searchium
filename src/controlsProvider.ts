9d301c09cfea9c6b1ef74bf750aa3bc272cac4f4
Robert Millar <robert.millar@cantab.net>
2023-03-25 19:52:54 -0700

    html tag function for IDE support

--- src/controlsProvider.ts
+++ src/controlsProvider.ts
@@ -13,6 +13,29 @@ export function getUri(webview: vscode.Webview, extensionUri: vscode.Uri, pathLi
     return webview.asWebviewUri(vscode.Uri.joinPath(extensionUri, ...pathList));
 }
 
+function html(strings: TemplateStringsArray, ...insertions: any[]) {
+    let s = "";
+    for (let i = 0; i < insertions.length; ++i) {
+        s += strings[i];
+        try {
+            let insertion = insertions[i];
+            if (insertion instanceof Object && insertion.toString === Object.prototype.toString) {
+                s += JSON.stringify(insertion, (key, value) => {
+                    if (typeof value === 'bigint') { return value.toString(); }
+                    else { return value; }
+                });
+            }
+            else {
+                s += `${insertion}`;
+            }
+        } catch {
+            s += "LOG_ERROR";
+        }
+    }
+    s += strings[strings.length - 1];
+    return s;
+}
+
 export class ControlsProvider implements vscode.WebviewViewProvider {
     constructor(private readonly extensionUri: vscode.Uri) { }
     public resolveWebviewView(
@@ -32,27 +55,30 @@ export class ControlsProvider implements vscode.WebviewViewProvider {
         const codiconsUri = getUri(webview, extensionUri, ['node_modules', '@vscode/codicons', 'dist', 'codicon.css']);
         const stylesheetUri = getUri(webview, extensionUri, ["out", "style.css"]);
         const nonce = getNonce();
-        return /*html*/ `
-          <!DOCTYPE html>
-          <html lang="en">
-            <head>
-              <meta charset="UTF-8">
-              <meta name="viewport" content="width=device-width, initial-scale=1.0">
-              <link href="${codiconsUri}" rel="stylesheet" />
-              <link href="${stylesheetUri}" rel="stylesheet" />
-              <title>Hello World!</title>
-            </head>
-            <body>
-                <vscode-text-field class="search-input">
-                    <span slot="start" class="codicon codicon-search" />
-                </vscode-text-field>
-                <br>
-                <vscode-text-field class="search-input">
-                    <span slot="start" class="codicon codicon-folder" />
-                </vscode-text-field>
-              <script type="module" nonce="${nonce}" src="${webviewUri}"></script>
-            </body>
-          </html>
+        return html`
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="${codiconsUri}" rel="stylesheet" />
+    <link href="${stylesheetUri}" rel="stylesheet" />
+    <title>Hello World!</title>
+</head>
+
+<body>
+    <vscode-text-field class="search-input">
+        <span slot="start" class="codicon codicon-search" />
+    </vscode-text-field>
+    <br>
+    <vscode-text-field class="search-input">
+        <span slot="start" class="codicon codicon-folder" />
+    </vscode-text-field>
+    <script type="module" nonce="${nonce}" src="${webviewUri}"></script>
+</body>
+
+</html>
         `;
     }
 }
