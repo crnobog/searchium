342685e395bd71dd2515d3e191c21efeba29975a
Robert Millar <robert.millar@cantab.net>
2023-03-26 17:44:33 -0700

    Update documentRegistrationService.ts
    Register workspace folders with search server

--- src/documentRegistrationService.ts
+++ src/documentRegistrationService.ts
@@ -20,6 +20,14 @@ export class DocumentRegistrationService implements vscode.Disposable {
     }
 
     public async run() {
+        if (vscode.workspace.workspaceFolders) {
+            for (let folder of vscode.workspace.workspaceFolders) {
+                if (folder.uri.scheme === 'file') {
+                    this.registerPath(folder.uri.fsPath);
+                }
+            }
+        }
+        vscode.workspace.onDidChangeWorkspaceFolders(this.onWorkspaceFoldersChanged, this);
         for (const editor of vscode.window.visibleTextEditors) {
             const uri = editor.document.uri;
             if (uri.scheme !== 'file') {
@@ -31,6 +39,19 @@ export class DocumentRegistrationService implements vscode.Disposable {
             vscode.workspace.onDidOpenTextDocument(this.onTextDocumentOpened, this),
             vscode.workspace.onDidCloseTextDocument(this.onTextDocumentClosed, this)
         );
+    }
+
+    private onWorkspaceFoldersChanged(event: vscode.WorkspaceFoldersChangeEvent) {
+        for (let added of event.added) {
+            if (added.uri.scheme === 'file') {
+                this.registerPath(added.uri.fsPath);
+            }
+        }
+        for (let removed of event.removed) {
+            if (removed.uri.scheme === 'file') {
+                this.unregisterPath(removed.uri.fsPath);
+            }
+        }
     }
 
     private onTextDocumentOpened(doc: vscode.TextDocument) {
